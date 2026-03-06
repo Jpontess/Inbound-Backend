29b704b4e6944c7d219904cf2903fd6e4ee76203
Caio Poyares <capoyares@gmail.com>
2026-03-06 17:11:40 -0300

    conflicts resolved

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,25 +1,35 @@
 import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
-import { SupplierModule } from './supplier/supplier.module';
-import { ReceiptModule } from './receipt/receipt.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { AuthModule } from './auth/auth.module';
+import { APP_GUARD } from '@nestjs/core';
+import { MongooseModule } from '@nestjs/mongoose';
+
+import { AuthModule } from './common/auth/auth.module';
+import { AuthenticationGuard } from './common/auth/guards/authentication.guard';
+import { ReceiptModule } from './receipt/receipt.module';
+import { SupplierModule } from './supplier/supplier.module';
 
 @Module({
   imports: [
+    AuthModule,
     ConfigModule.forRoot({
       isGlobal: true,
     }),
     MongooseModule.forRootAsync({
       imports: [ConfigModule],
       useFactory: (configService: ConfigService) => ({
-        uri: configService.get<string>('MONGO_URI'),
+        uri: configService.getOrThrow<string>('MONGO_URI'),
       }),
       inject: [ConfigService],
     }),
     AuthModule,
     SupplierModule,
     ReceiptModule,
+  ],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: AuthenticationGuard,
+    },
   ],
 })
 export class AppModule {}
