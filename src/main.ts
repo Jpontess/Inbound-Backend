1c68d38d3551e8d522df34ec13dec0fed18cf01a
joao pontes <Joo>
2026-05-23 13:59:51 -0300

    Alterando porta para o ENV

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,6 @@ async function bootstrap() {
       transform: true, // Transforma os tipos de acordo com as definições do DTO
     }),
   );
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT || 3000);
 }
 void bootstrap();
